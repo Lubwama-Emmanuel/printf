6318ad9df2a3f5bffd437ff0ad535a5fc94d8880
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-10-16 02:58:57 -0700

    feat: _printf function

---
+++ _printf.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+/**
+ * _printf - variadic function that works as printf
+ *
+ * @format: data to print
+ *
+ * Return: returns the printed string
+ */
+int _printf(const char *format, ...)
+{
+	int j = 0, i = 0;
+	char buff[100];
+	char *s;
+
+	va_list(ap);
+	va_start(ap, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+				{
+					buff[j] = (char)va_arg(ap, int);
+					j++;
+					break;
+				}
+				case 's':
+				{
+					s = va_arg(ap, char*);
+					strcpy(&buff[j], s);
+					j += strlen(s);
+					break;
+				}
+			}
+		}
+		else
+		{
+			buff[j] = format[i];
+			j++;
+		}
+		i++;
+	}
+	fwrite(buff,  j, 1, stdout);
+	va_end(ap);
+	return (j);
+}
