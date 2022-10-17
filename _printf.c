d533363670acd0bffc7f742302d018c099650fcb
Samuelibudi <ibudisamuel@gmail.com>
2022-10-17 08:41:06 -0700

    Restoring repo

--- _printf.c
+++ _printf.c
@@ -9,44 +9,43 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+	int j = 0, i = 0;
+	char buff[1024];
+	char *s;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
-		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
+	va_list(ap);
+	va_start(ap, format);
+
+	while (format && format[i])
 	{
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+				{
+					buff[j] = (char)va_arg(ap, int);
+					j++;
+					break;
+				}
+				case 's':
+				{
+					s = va_arg(ap, char*);
+					strcpy(&buff[j], s);
+					j += strlen(s);
+					break;
+				}
 			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
 		}
 		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
+		{
+			buff[j] = format[i];
+			j++;
+		}
+		i++;
 	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+	fwrite(buff,  j, 1, stdout);
+	va_end(ap);
+	return (j);
 }
