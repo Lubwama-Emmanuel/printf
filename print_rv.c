1eb846b0a3e221eaae3b42bdb2afd9523194e304
Samuelibudi <ibudisamuel@gmail.com>
2022-10-18 13:05:43 -0700

    Adding file to print string in reverse

---
+++ print_rv.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_rv - prints string in reverse
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer.
+ * Return: number of char printed.
+ */
+int print_rv(va_list arguments, char *buf, unsigned int ibuf)
+{
+	char *str;
+	unsigned int i;
+	int j = 0;
+	char nil[] = "(llun)";
+
+	str = va_arg(arguments, char *);
+	if (str == NULL)
+	{
+		for (i = 0; nil[i]; i++)
+			ibuf = handl_buf(buf, nill[i], ibuf);
+		return (6);
+	}
+	for (i = 0; str[i]; i++)
+		;
+	j = i - 1;
+	for (; j >= 0; j--)
+	{
+		ibuf = handl_buf(buf, str[j], ibuf);
+	}
+	return (i);
+}
