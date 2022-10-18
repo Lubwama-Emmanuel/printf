64af21ade9f188a1e07012bcabb898254f54235a
Samuelibudi <ibudisamuel@gmail.com>
2022-10-18 07:35:35 -0700

    Fixing file main.h

--- main.h
+++ main.h
@@ -20,5 +20,6 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
+int print_ctb(va_list arguments, char *buf, unsigned int ibuf);
 
 #endif
